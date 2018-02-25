544e9ed66823165ebbd54da3bcd81a7b97f392e6
Daibogh <DaiBogh@gmail.com>
2018-02-25 13:41:18 +0300

    fuckyou

--- python_utils/add_image.py
+++ python_utils/add_image.py
@@ -6,7 +6,8 @@ pprint = pp.pprint
 post_content = '''---
 layout: default
 ---
-![Picture example](images/kvartirnik_photos/{}.jpg)
+![Picture example](https://github.com/kvartirnik/website/blob/gh-pages/images/kvartirnik_photos/{}.jpg)
+
 
 '''
 
